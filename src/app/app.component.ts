bfc498314fd718db264cf016ac0f5d17f0e169a0
Rachel Eiting <reiting1@gmail.com>
2020-03-12 18:10:50 -0500

    created angular todo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,30 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+
 })
 export class AppComponent {
-  title = 'todo-app';
+  title: string = 'todo-app';
+  todoArray: Array<string> = [];
+
+  addTodo(value) {
+    this.todoArray.push(value)
+  }
+
+  deleteItem(todo) {
+    for (let i = 0; i <= this.todoArray.length; i++) {
+      if (todo == this.todoArray[i]) {
+        this.todoArray.splice(i, 1)
+      }
+    }
+  }
+  todoSubmit(value: any) {
+    if (value !== "") {
+      this.todoArray.push(value.todo)
+    } else {
+      alert('Field required **')
+    }
+  }
 }
+
+
